f691e109538644c0d8521db595f39d910df99737
ArniAlex <aleksandraselest383@gmail.com>
2025-03-31 02:44:44 +0300

    Update 5z.h

--- 5z.h
+++ 5z.h
@@ -5,7 +5,7 @@ public:
 	virtual void add_line(const char* str, int key) = 0; 
 	virtual char* search_value() const = 0;
 	virtual void delete_value() = 0;
-	virtual priority_queue& merge(const priority_queue& first, const priority_queue& second) = 0;
+	virtual priority_queue& merge(const priority_queue& second) = 0;
 	virtual ~priority_queue() noexept = default;
 };
 
