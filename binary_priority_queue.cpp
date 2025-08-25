074cbb1aacc65dcf2d6860551f9c041502bf0239
ArniAlex <aleksandraselest383@gmail.com>
2025-08-25 17:29:02 +0300

    Update binary_priority_queue.cpp

--- binary_priority_queue.cpp
+++ binary_priority_queue.cpp
@@ -1,236 +1,226 @@
-/*
-ЕЩЁ В ПРОЦЕССЕ
-*/
-
 #include <iostream>
-#include "5z.h"  
-#include <stdexcept> 
-#include <cstring> 
+#include <cstring>
 
-class binary_priority_queue : public priority_queue {
-	/*
-	 * структура
-	 * массив
-	 * методы
-	 */
-	struct node {
-		int key;
-		char* value;
-	};
-	node* heap;
-	int current_size;
-	int max_size;
+//РќРђРЎР›Р•Р”РћР’РђРќРР• Р РџР РђР’РР›Рћ РџРЇРўР
 
-	//вспомогательные методы
-	void heapify_up() noexept {
-		for (int i = current_size - 1; i > 0; --i) {
-			int par_i = (i - 1) / 2;
-			if (heap[i].key > heap[par_i].key) {
-				swap(i, par_i);
-			}
-			else {
-				break; 
-			}
-		}
-	};
+#pragma warning (disable: 4996)  
 
-	void heapify_down(int ind) noexept {
-		int left = 2 * ind + 1, right = 2 * ind + 2;  
-		int max = ind;
+class binary_priority_queue final {
+    struct node {
+        int priority;
+        char* value;
 
-		if (left < size && heap[left].key > heap[max].key) {
-			max = left; 
-		}
+        node() : priority(0), value(nullptr) {};
+        ~node() {
+            delete[] value;
+        }; 
+    };
 
-		if (right < size && heap[right].key > heap[max].key) {
-			max = right;
-		}
+    node* heap;
+    int current_size;
+    int max_size;
 
-		if (largest != ind) {
-			swap(ind, max);  
-			heapifyDown(max);  
-		}
-	};
+private:
+    void heapify_up(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heap[parent].priority < heap[index].priority)
+                break;
+            swap(parent, index);
+            index = parent;
+        }
+    }
 
-	void swap(int i1, int i2) {
-		node temp;
-		temp = heap[i1];
-		heap[i1] = heap[i2];
-		heap[i2] = temp;
-	};
-	
+    void heapify_down(int index) {
+        while (true) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int largest = index;
+
+            if (left < current_size && heap[left].priority > heap[largest].priority)
+                largest = left;
+            if (right < current_size && heap[right].priority > heap[largest].priority)
+                largest = right;
+
+            if (largest == index)
+                break;
+
+            swap(index, largest);
+            index = largest;
+        }
+    }
+
+    void swap(int first, int second) {
+        node temp = heap[first];
+        heap[first] = heap[second];
+        heap[second] = temp;
+    }
+
+    void swap(binary_priority_queue& other) {
+        node* temp_heap = heap;
+        heap = other.heap;
+        other.heap = temp_heap;
+
+        int temp_size = current_size;
+        current_size = other.current_size;
+        other.current_size = temp_size;
+
+        int temp_max = max_size;
+        max_size = other.max_size;
+        other.max_size = temp_max;
+    }
+
+    void copy_nodes(const node* source, int count) {
+        for (int i = 0; i < count; ++i) {
+            if (source[i].value) {
+                heap[i].priority = source[i].priority;
+                heap[i].value = new char[strlen(source[i].value) + 1];
+                strcpy(heap[i].value, source[i].value);
+            }
+        }
+    }
+
 public:
-	binary_priority_queue(int initial_size) : max_size(initial_size), current_size(0) {
-		heap = new node[max_size];
+    binary_priority_queue(int initial_size)
+        : max_size(initial_size > 0 ? initial_size : throw "Invalid size"),
+        current_size(0),
+        heap(new node[max_size]()) {}
 
-		if (heap == nullptr) { throw std::bad_alloc(); } //обязательно ли вообще
+    binary_priority_queue(const binary_priority_queue& other)
+        : max_size(other.max_size),
+        current_size(other.current_size),
+        heap(new node[max_size]) {
+        copy_nodes(other.heap, current_size);
+    }
 
-		for (int i = 0; i < max_size; ++i) {
-			heap[i].key = 0;
-			heap[i].value = nullptr;
-		}
-	}; 
+    binary_priority_queue& operator=(const binary_priority_queue& other) {
+        if (this != &other) {
+            binary_priority_queue temp(other);
+            swap(temp);
+        }
+        return *this;
+    }
 
-	binary_priority_queue(const binary_priority_queue& other): heap(nullptr), max_size(other.max_size), current_size(other.current_size) {
-		heap = new node[other.max_size];
-		if (heap == nullptr) { throw std::bad_alloc(); }
+    binary_priority_queue(binary_priority_queue&& other) noexcept
+        : heap(other.heap),
+        max_size(other.max_size),
+        current_size(other.current_size) {
+        other.heap = nullptr;
+        other.max_size = 0;
+        other.current_size = 0;
+    }
 
-		for (int i = 0; i < current_size; ++i) {
-			heap[i].key = other.heap[i].key;
+    binary_priority_queue& operator=(binary_priority_queue&& other) noexcept {
+        if (this != &other) {
+            delete[] heap;
+            heap = other.heap;
+            max_size = other.max_size;
+            current_size = other.current_size;
+            other.heap = nullptr;
+            other.max_size = 0;
+            other.current_size = 0;
+        }
+        return *this;
+    }
 
-			if (other.heap[i].value != nullptr) {
-				int len = strlen(other.heap[i].value);
-				heap[i].value = new char[len + 1];
-				if (heap[i].value == nullptr) {
-					for (int j = 0; j < i; ++j) {
-						delete[] heap[j].value;
-					}
-					delete[] heap;
-					heap = nullptr;
-					throw std::bad_alloc();
-				}
-				memcpy(heap[i].value, other.heap[i].value, len + 1);
-			}
-			else {
-				heap[i].value = nullptr;
-			}
-		}
-	};
+    ~binary_priority_queue() {
+        delete[] heap;
+    }
 
-	binary_priority_queue& operator=(const binary_priority_queue& other) {
-		if (this == &other) {
-			return *this;
-		}
+    bool is_empty() const { return current_size == 0; }
+    bool is_full() const { return current_size == max_size; }
 
-		for (int i = 0; i < current_size; ++i) {
-			delete[] heap[i].value;
-		}
-		delete[] heap;
+public:
+    void add_value(const char* str, int priority) {
+        if (!str) throw "Null pointer";
+        if (strlen(str) == 0) throw "Empty string";
 
-		max_size = other.max_size;
-		current_size = other.current_size;
+        size_t len = strlen(str);
+        heap[current_size].value = new char[len + 1];
+        strncpy(heap[current_size].value, str, len);
+        heap[current_size].value[len] = '\0';
+    }
 
-		heap = new node[max_size]; 
-		if (heap == nullptr) {
-			heap = nullptr;
-			max_size = 0;
-			current_size = 0;
-			throw std::bad_alloc();
-		}
+    const char* search_max() const {
+        if (is_empty()) throw "Queue is empty";
+        return heap[0].value;
+    }
 
-		for (int i = 0; i < current_size; ++i) {
-			heap[i].key = other.heap[i].key;
-			if (other.heap[i].value != nullptr) {
-				int len = strlen(other.heap[i].value); 
-				heap[i].value = new char[len + 1];
-				if (heap[i].value == nullptr) {
-					for (int j = 0; j < i; ++j) {
-						delete[] heap[j].value;
-					}
-					delete[] heap;
-					heap = nullptr;
-					max_size = 0;
-					current_size = 0;
-					throw std::bad_alloc();
-				}
-				memcpy(heap[i].value, other.heap[i].value, len + 1);
+    void delete_max() {
+        if (is_empty()) throw "Queue is empty";
 
-			}
-			else {
-				heap[i].value = nullptr;
-			}
-		}
+        delete[] heap[0].value;
 
-		return *this;
-	};
+        if (current_size == 1) {
+            current_size = 0;
+        }
+        else {
+            heap[0] = heap[current_size - 1]; 
+            current_size--;
+            heapify_down(0);
+        }
+    }
 
-	~binary_priority_queue() noexept override {
-		for (int i = 0; i < current_size; ++i) { 
-			delete[] heap[i].str;
-			heap[i].str = nullptr; 
-		}
-		delete[] heap;
-		heap = nullptr;
-	}; 
+    void merge(const binary_priority_queue& other) {
+        for (int i = 0; i < other.current_size; ++i) {
+            if (other.heap[i].value) {  // РЅРµ nullptr
+                add_value(other.heap[i].value, other.heap[i].priority);
+            }
+        }
+    }
 
+    binary_priority_queue meld(const binary_priority_queue& other) const {
+        binary_priority_queue result(current_size + other.current_size);
 
-	//основные методы
+        for (int i = 0; i < current_size; ++i) {
+            result.add_value(heap[i].value, heap[i].priority);
+        }
 
-	void add_line(const char* str, int key) override {
-		if (current_size == max_size) {
-			throw std::runtime_error("The queue is full.");
-		}
-		int ind = current_size;
+        for (int i = 0; i < other.current_size; ++i) {
+            result.add_value(other.heap[i].value, other.heap[i].priority);
+        }
 
-		heap[ind].key = key;
-		heap[ind].value = new char[strlen(str) + 1];
+        return result;
+    }
 
-		if (heap[ind].value == nullptr) { throw std::bad_alloc(); }
-
-		current_size += 1;
-
-		memcpy(heap[ind].value, str, strlen(str));
-
-		heapify_up();
-	};
-
-	char* search_value() override {
-		if (current_size == 0) {
-			throw std::runtime_error("The queue is empty.");
-		}
-		return heap[0].value; //возврат указателя?? чекнуть
-	};
-
-	void delete_value() override {
-		if (current_size == 0) {
-			throw std::runtime_error("The queue is empty.");
-		}
-
-		delete[] heap[0].value;
-
-		heap[0] = heap[current_size - 1]; 
-		current_size--;
-
-		heapify_down(0);
-	};
-
-	binary_priority_queue& merge(const binary_priority_queue& second) override {
-		if (this == &second || second.current_size == 0) {
-			return *this; 
-		}
-		
-		if (current_size == 0) {
-			*this = second;
-		} 
-		
-		if (max_size < current_size + second.current_size) {
-			node* newHeap = new node[current_size + second.current_size];
-			if (newHeap == nullptr) {
-				throw std::bad_alloc();
-			}
-
-			for (size_t i = 0; i < current_size; ++i) {
-				newHeap[i].key = heap[i].key;
-				if (heap[i].value != nullptr) {
-					int len = strlen(heap[i].value);
-					newHeap[i].value = new char[len + 1];
-					if (newHeap[i].value == nullptr) {
-						throw std::bad_alloc();
-					}
-					memcpy(newHeap[i].value, heap[i].value, len + 1);
-				}
-				else {
-					newHeap[i].value = nullptr;
-				}
-			}
-		}
-		
-
-		//TODO глубокое копирование надо..... 
-		memcpy(heap + current_size, second.heap, second.current_size);
-		heapify_down(current_size - 1);
-		current_size += second.current_size;
-	};
-	
+    friend std::ostream& operator<<(std::ostream& os, const binary_priority_queue& queue) {
+        os << "Queue (size: " << queue.current_size << "/" << queue.max_size << "):\n";
+        for (int i = 0; i < queue.current_size; ++i) {
+            os << "  [" << i << "] Priority: " << queue.heap[i].priority
+                << ", Value: " << (queue.heap[i].value ? queue.heap[i].value : "null") << "\n";
+        }
+        return os;
+    }
 };
 
+int main() {
+    try {
+        binary_priority_queue q1(5), q2(5);
+
+        q1.add_value("a", 10);
+        q1.add_value("b", 20);
+
+        q2.add_value("c", 5);
+        q2.add_value("d", 30);
+        q2.add_value("e", 15);
+
+        std::cout << "Queue 1:\n" << q1 << "\n";
+        std::cout << "Queue 2:\n" << q2 << "\n";
+
+        std::cout << "Max in q1: " << q1.search_max() << "\n";
+        q1.delete_max();
+        std::cout << "After delete max from q1:\n" << q1 << "\n";
+
+        q1.merge(q2);
+        std::cout << "After merge q2 to q1:\n" << q1 << "\n";
+
+        auto q3 = q1.meld(q2);
+        std::cout << "After meld q1 and q2 (q3):\n" << q3 << "\n";
+
+    }
+    catch (const char* msg) {
+        std::cerr << "Error: " << msg << "\n";
+    }
+
+    return 0;
+}
